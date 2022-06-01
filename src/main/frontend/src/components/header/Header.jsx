9798d408ec404713de544e7293cfd059b4fafb91
mrshyian <shyvlad1994@gmail.com>
2022-06-01 13:05:59 +0200

    Added onClick to buttons "My profile", "Forum", "Main page"

--- src/main/frontend/src/components/header/Header.jsx
+++ src/main/frontend/src/components/header/Header.jsx
@@ -3,12 +3,25 @@ import Button from "../button/Button";
 import './Header.css'
 
 const Header = (inSession) => {
+
+    const renderToMyProfilePage = () => {
+    //TODO: methode to render to "My profile" page
+    }
+
+    const renderToForumPage = () => {
+    //TODO: methode to render to "Forum" page
+    }
+
+    const renderToMainPage = () => {
+    //TODO: methode to render to "Main page" page
+    }
+
     return (
         <div className="header-box">
             <span className="left-buttons">
-                <Button>My profile</Button>
-                <Button>Forum</Button>
-                <Button>Main page</Button>
+                <Button onClick={renderToMyProfilePage}>My profile</Button>
+                <Button onClick={renderToForumPage}>Forum</Button>
+                <Button onClick={renderToMainPage}>Main page</Button>
             </span>
             {inSession===true ? <span className="right-buttons"><Button>Logout</Button></span> : <span className="right-buttons"><Button>Login</Button> <Button>Registration</Button></span>}
         </div>
