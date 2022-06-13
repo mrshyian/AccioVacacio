018096f0931be14f132cd9ad612705e2a81024dc
mrshyian <shyvlad1994@gmail.com>
2022-06-13 22:50:14 +0200

    added CSS to living coasts API

--- src/main/frontend/src/components/searchCity/livingCosts/LivingCoasts.jsx
+++ src/main/frontend/src/components/searchCity/livingCosts/LivingCoasts.jsx
@@ -1,36 +1,53 @@
 import React from 'react';
 import "./livingCosts.css"
+import { Card} from "react-bootstrap";
 
 const LivingCosts = (props) => {
     return (
         <div className="living-costs">
-            <h1 style={{textAlign: "center", color: "coral"}}>Living Costs</h1>
+            <h1 style={{textAlign: "center", color: "orange"}}>Living Costs</h1>
             <div style={{display: "flex"}}>
-                <div className="dupa-for-living-cost">
-                    <div className="back-for-living-cost">
-                        {props.livingCosts.slice(0, 8).map((livingCosts, index) => {
-                            return (
-                                <div key={index} className="space-between">
-                                    <div>{livingCosts.itemName}</div>
-                                    <div>{livingCosts.averagePrice} {livingCosts.cost}</div>
-                                </div>
-                            )
-                        })}
+                <Card
+                    bg={"dark"}
+                    key={"dark"}
+                    text={'white'}
+                    style={{width: '45rem', margin: "5px"}}
+                    className="mb-2"
+                >
+                    <Card.Body>
+                        <Card.Text>
+                            {props.livingCosts.slice(0, 8).map((livingCosts, index) => {
+                                return (
+                                    <div key={index} className="space-between">
+                                        <div>{livingCosts.itemName}</div>
+                                        <div>{livingCosts.averagePrice} {livingCosts.cost}</div>
+                                    </div>
+                                )
+                            })}
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
 
-                    </div>
-                </div>
-                <div className="dupa-for-living-cost">
-                    <div className="back-for-living-cost">
-                        {props.livingCosts.slice(8, 16).map((livingCosts, index) => {
-                            return (
-                                <div key={index} className="space-between">
-                                    <div>{livingCosts.itemName}</div>
-                                    <div>{livingCosts.averagePrice} {livingCosts.cost}</div>
-                                </div>
-                            )
-                        })}
-                    </div>
-                </div>
+                <Card
+                    bg={"dark"}
+                    key={"dark"}
+                    text={'white'}
+                    style={{width: '45rem', margin: "5px"}}
+                    className="mb-2"
+                >
+                    <Card.Body>
+                        <Card.Text>
+                            {props.livingCosts.slice(8, 16).map((livingCosts, index) => {
+                                return (
+                                    <div key={index} className="space-between">
+                                        <div>{livingCosts.itemName}</div>
+                                        <div>{livingCosts.averagePrice} {livingCosts.cost}</div>
+                                    </div>
+                                )
+                            })}
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
             </div>
         </div>
     )
