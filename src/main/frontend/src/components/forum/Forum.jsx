1262cf8f07aa3e13cd3a3552bcf89e9a1133568f
JakubGem <jakegemb89@gmail.com>
2022-06-14 17:37:27 +0200

    Created main forum site component with css

--- src/main/frontend/src/components/forum/Forum.jsx
+++ src/main/frontend/src/components/forum/Forum.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import Header from "../header/Header";
 import ForumLeftPanel from "./forumleftpanel/ForumLeftPanel";
+import ForumRightPanel from "./forumRightPanel/ForumRightPanel";
+
 
 const Forum = () => {
     return (
         <div>
             <Header/>
-            <ForumLeftPanel/>
+            <div style={{display: "flex"}}>
+                <ForumLeftPanel/>
+                <ForumRightPanel/>
+            </div>
+
         </div>
     );
 };
