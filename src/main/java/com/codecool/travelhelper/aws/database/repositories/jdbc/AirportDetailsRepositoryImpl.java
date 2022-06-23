1be089346e8454453be6aeeb12845415d17e335d
mrshyian <shyvlad1994@gmail.com>
2022-06-24 01:16:59 +0200

    Airport details update is ready

--- src/main/java/com/codecool/travelhelper/aws/database/repositories/jdbc/AirportDetailsRepositoryImpl.java
+++ src/main/java/com/codecool/travelhelper/aws/database/repositories/jdbc/AirportDetailsRepositoryImpl.java
@@ -1,4 +1,49 @@
 package com.codecool.travelhelper.aws.database.repositories.jdbc;
 
+import com.codecool.travelhelper.aws.database.models.AirportDetailsTable;
+import com.codecool.travelhelper.aws.database.models.WeatherTable;
+import com.codecool.travelhelper.aws.database.repositories.AirportDetailsRepository;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+@NoArgsConstructor
+@Getter
 public class AirportDetailsRepositoryImpl {
+
+    @Autowired
+    AirportDetailsRepository airportDetailsRepository;
+
+    public void setAirportDetailsDataByCityAndCountryName(AirportDetailsTable airportDetailsTable) {
+        AirportDetailsTable updatedResponseObject;
+        String cityName = airportDetailsTable.getCityName();
+        String countryName = airportDetailsTable.getCountryName();
+
+        Optional<AirportDetailsTable> response = airportDetailsRepository.findAllByCityNameAndCountryName(cityName, countryName);
+
+        if (response.isPresent()){
+            updatedResponseObject = response.get();
+            updateResponseObject(airportDetailsTable, updatedResponseObject);
+        } else {
+            updatedResponseObject = airportDetailsTable;
+        }
+
+        airportDetailsRepository.save(updatedResponseObject);
+    }
+
+    private void updateResponseObject(AirportDetailsTable newObject, AirportDetailsTable objectFromDB){
+        objectFromDB.setAirportName(newObject.getAirportName());
+        objectFromDB.setAirportLocation(newObject.getAirportLocation());
+        objectFromDB.setAirportStreetName(newObject.getAirportStreetName());
+        objectFromDB.setAirportStreetNumber(newObject.getAirportStreetNumber());
+        objectFromDB.setAirportState(newObject.getAirportState());
+        objectFromDB.setAirportCity(newObject.getAirportCity());
+        objectFromDB.setAirportPhoneNumber(newObject.getAirportPhoneNumber());
+        objectFromDB.setAirportWebsite(newObject.getAirportWebsite());
+
+    }
 }
