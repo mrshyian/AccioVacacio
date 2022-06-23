25eb531642c7db230043faa7851b9df3937f6b9e
mrshyian <shyvlad1994@gmail.com>
2022-06-24 01:25:56 +0200

    Added annotations to LivingCostsRepositoryImpl and WorldNewsRepositoryImpl

--- src/main/java/com/codecool/travelhelper/aws/database/repositories/jdbc/LivingCostsRepositoryImpl.java
+++ src/main/java/com/codecool/travelhelper/aws/database/repositories/jdbc/LivingCostsRepositoryImpl.java
@@ -1,4 +1,18 @@
 package com.codecool.travelhelper.aws.database.repositories.jdbc;
 
+import com.codecool.travelhelper.aws.database.repositories.LivingCostsRepository;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@NoArgsConstructor
+@Getter
 public class LivingCostsRepositoryImpl {
+
+    @Autowired
+    LivingCostsRepository livingCostsRepository;
+
+
 }
