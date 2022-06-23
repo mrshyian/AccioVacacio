ede9ac601a7beb52ccee03ba1946f165c0593d4c
Tetiana Shcherbak <shcherbak.tet@gmail.com>
2022-06-23 13:55:35 +0200

    Fixed webSite rendering on Attractions cards

--- src/main/java/com/codecool/travelhelper/API/rapidapi/controller/apicontroller/WeatherController.java
+++ src/main/java/com/codecool/travelhelper/API/rapidapi/controller/apicontroller/WeatherController.java
@@ -4,10 +4,7 @@ import com.codecool.travelhelper.API.rapidapi.model.apimodel.WeatherApiModel;
 import com.codecool.travelhelper.API.rapidapi.service.apiservice.WeatherService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,6 +16,12 @@ public class WeatherController {
     @GetMapping("/")
     public String test() {
         return "test";
+    }
+
+    @PostMapping("/registration")
+    String addDada(@RequestBody String data) {
+        System.out.println("post mapping check");
+        return "passed";
     }
 
     @GetMapping("/weather/{cityName}/{countryName}")
