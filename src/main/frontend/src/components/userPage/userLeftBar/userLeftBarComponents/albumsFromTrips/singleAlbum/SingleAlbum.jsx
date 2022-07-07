9fa05e6a892dfbbc3b74f71927790123fa71c7e1
mrshyian <shyvlad1994@gmail.com>
2022-07-07 12:55:42 +0200

    max character in album description is 55

--- src/main/frontend/src/components/userPage/userLeftBar/userLeftBarComponents/albumsFromTrips/singleAlbum/SingleAlbum.jsx
+++ src/main/frontend/src/components/userPage/userLeftBar/userLeftBarComponents/albumsFromTrips/singleAlbum/SingleAlbum.jsx
@@ -6,11 +6,9 @@ import DeleteAlbumModal from "../../../../../modals/deleteAlbumModal/DeleteAlbum
 import {useNavigate} from "react-router-dom";
 
 
-
 const SingleAlbum = (props) => {
     const navigate = useNavigate();
     const [modalOpen, setModalOpen] = useState(false);
-
 
     return (
         <Card
@@ -24,15 +22,29 @@ const SingleAlbum = (props) => {
                     setModalOpen(true)
                 }} variant="dark" className="delete-album-btn">{<FaTimes/>}</Button>
                 <div>
-                    <Card.Img className="album-image" variant="top" src="https://neapol.pl/wp-content/uploads/2022/03/wybrzeze-sorrento-wlochy_shutterstock_519559345-1.jpg" />
+                    <Card.Img className="album-image" variant="top"
+                              src="https://neapol.pl/wp-content/uploads/2022/03/wybrzeze-sorrento-wlochy_shutterstock_519559345-1.jpg"/>
                     <Card.Body>
-                        <Card.Title><div>{props.album.albumName}</div><div className="album-date-and-place">{props.album.city}/{props.album.country} - {props.album.tripDate}</div></Card.Title>
-                        <Card.Text>
-                            {props.album.aboutAlbum}
-                        </Card.Text>
-                        <Button onClick={()=> navigate("/userpage/albums_from_trips/album", {state: {
+                        <Card.Title>
+                            <div>{props.album.albumName}</div>
+                            <div
+                                className="album-date-and-place">{props.album.city}/{props.album.country} - {props.album.tripDate}</div>
+                        </Card.Title>
+                        {props.album.aboutAlbum.length < 55 ? (
+                            <Card.Text>
+                                {props.album.aboutAlbum}
+                            </Card.Text>
+                        ) : (
+                            <Card.Text>
+                                {props.album.aboutAlbum.slice(0, 55)}...
+                            </Card.Text>
+                        )}
+
+                        <Button onClick={() => navigate("/userpage/albums_from_trips/album", {
+                            state: {
                                 album: props.album
-                                }})}
+                            }
+                        })}
                                 variant="outline-warning">Go to album
                         </Button>
                     </Card.Body>
