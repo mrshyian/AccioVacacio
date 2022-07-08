711e25639675eed5c60ee481ff7488d40f82bf7d
mrshyian <shyvlad1994@gmail.com>
2022-07-08 02:28:25 +0200

    booking API is ready

--- src/main/java/com/codecool/travelhelper/API/rapidapi/controllers/BookingApiController.java
+++ src/main/java/com/codecool/travelhelper/API/rapidapi/controllers/BookingApiController.java
@@ -1,9 +1,12 @@
 package com.codecool.travelhelper.API.rapidapi.controllers;
 
+import com.codecool.travelhelper.API.rapidapi.models.BookingApiModel;
 import com.codecool.travelhelper.API.rapidapi.services.BookingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,8 +17,8 @@ public class BookingApiController {
     @Autowired
     BookingService bookingService;
 
-//    @GetMapping("/booking/{cityName}/{countryName}")
-//    public BookingApiModel getBooking(@PathVariable String cityName, @PathVariable String countryName){
-//        return bookingService.getBooking(cityName, countryName);
-//    }
+    @GetMapping("/booking/{cityName}/{countryName}")
+    public BookingApiModel getBooking(@PathVariable String cityName, @PathVariable String countryName){
+        return bookingService.getBooking(cityName, countryName);
+    }
 }
